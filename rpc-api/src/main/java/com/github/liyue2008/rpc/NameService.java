8769a1e47f2f1f8aabf9bb5131620fc687bb968f
liyue25 <liyue25@jd.com>
2019-09-23 22:43:29 +0800

    增加了rpc-api的注释。

--- rpc-api/src/main/java/com/github/liyue2008/rpc/NameService.java
+++ rpc-api/src/main/java/com/github/liyue2008/rpc/NameService.java
@@ -17,10 +17,22 @@ import java.io.IOException;
 import java.net.URI;
 
 /**
+ * 注册中心
  * @author LiYue
  * Date: 2019/9/20
  */
 public interface NameService {
+    /**
+     * 注册服务
+     * @param serviceName 服务名称
+     * @param uri 服务地址
+     */
     void registerService(String serviceName, URI uri) throws IOException;
+
+    /**
+     * 查询服务地址
+     * @param serviceName 服务名称
+     * @return 服务地址
+     */
     URI lookupService(String serviceName) throws IOException;
 }
--- rpc-api/src/main/java/com/github/liyue2008/rpc/RpcAccessPoint.java
+++ rpc-api/src/main/java/com/github/liyue2008/rpc/RpcAccessPoint.java
@@ -17,11 +17,32 @@ import java.io.Closeable;
 import java.net.URI;
 
 /**
+ * RPC框架对外提供的服务接口
  * @author LiYue
  * Date: 2019/9/20
  */
 public interface RpcAccessPoint extends Closeable{
+    /**
+     * 客户端获取远程服务的引用
+     * @param uri 远程服务地址
+     * @param serviceClass 服务的接口类的Class
+     * @param <T> 服务接口的类型
+     * @return 远程服务引用
+     */
     <T> T getRemoteService(URI uri, Class<T> serviceClass);
+
+    /**
+     * 服务端注册服务的实现实例
+     * @param service 实现实例
+     * @param serviceClass 服务的接口类的Class
+     * @param <T> 服务接口的类型
+     * @return 服务地址
+     */
     <T> URI addServiceProvider(T service, Class<T> serviceClass);
+
+    /**
+     * 服务端启动RPC框架，监听接口，开始提供远程服务。
+     * @return 服务实例，用于程序停止的时候安全关闭服务。
+     */
     Closeable startServer() throws Exception;
 }
