682bd6bcd6ab1982b4d4b75be0654b09c216e5f4
liyue25 <liyue25@jd.com>
2019-09-28 11:32:15 +0800

    增加注释

--- rpc-netty/src/main/java/com/github/liyue2008/rpc/client/DynamicStubFactory.java
+++ rpc-netty/src/main/java/com/github/liyue2008/rpc/client/DynamicStubFactory.java
@@ -47,18 +47,23 @@ public class DynamicStubFactory implements StubFactory{
     @SuppressWarnings("unchecked")
     public <T> T createStub(Transport transport, Class<T> serviceClass) {
         try {
+            // 填充模板
             String stubSimpleName = serviceClass.getSimpleName() + "Stub";
             String classFullName = serviceClass.getName();
             String stubFullName = "com.github.liyue2008.rpc.client.stubs." + stubSimpleName;
             String methodName = serviceClass.getMethods()[0].getName();
 
             String source = String.format(STUB_SOURCE_TEMPLATE, stubSimpleName, classFullName, methodName, classFullName, methodName);
+            // 编译源代码
             JavaStringCompiler compiler = new JavaStringCompiler();
             Map<String, byte[]> results = compiler.compile(stubSimpleName + ".java", source);
+            // 加载编译好的类
             Class<?> clazz = compiler.loadClass(stubFullName, results);
 
+            // 把Transport赋值给桩
             ServiceStub stubInstance = (ServiceStub) clazz.newInstance();
             stubInstance.setTransport(transport);
+            // 返回这个桩
             return (T) stubInstance;
         } catch (Throwable t) {
             throw new RuntimeException(t);
